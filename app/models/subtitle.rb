08dde9dd8320dea918bfa104b963593224125fbb
sun <jmw686@nyu.edu>
2019-01-03 14:41:45 -0600

    add srt file creation methods

--- app/models/subtitle.rb
+++ app/models/subtitle.rb
@@ -14,5 +14,21 @@ class Subtitle < ApplicationRecord
   has_many :captions, dependent: :destroy
   has_one :video, dependent: :destroy
 
-  validates :complete_text, presence: true
+
+  def create_file
+    File.open("out.txt", "w") do |f|
+      f.write(this.get_complete_text )
+    end
+  end
+
+    def get_complete_text
+      output = ""
+      captions.each do |caption|
+        output += caption.caption_order + "\n"
+        output += caption.start_time + "-->" + caption.end_time + "\n"
+        output += caption.text + "\n\n"
+
+      end
+      output
+    end
 end
